fc29cb70bcb473b2282757e1cd7515a18713a477
LauVii <viniciusglaureano@gmail.com>
2024-10-17 20:18:16 -0300

    aula 10 finalizada

---
+++ JS/Modulo1/10-recursos-nativo-navegador/script.js
@@ -0,0 +1,14 @@
+let Name = "Vinícius";
+
+console.log(Name);
+console.error(Name);
+console.warn(Name);
+
+window.alert(Name);
+Name = window.prompt("Insira o seu Nome: ");
+console.log(Name);
+
+let confirmation = window.confirm("Confirma essa ação?");
+console.log(confirmation);
+
+alert("Hello World!");
