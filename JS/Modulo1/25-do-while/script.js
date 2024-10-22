f68d71d3588115bfc9bc5be5919c2204d5895098
LauVii13 <viniciusglaureano@gmail.com>
2024-10-22 17:12:18 -0300

    aula 25 finished

---
+++ JS/Modulo1/25-do-while/script.js
@@ -0,0 +1,8 @@
+let velocity = 0;
+
+do {
+  alert("Velocidade do veículo = " + velocity + "Km/h");
+  velocity -= 20;
+} while (velocity > 0);
+
+alert("Velocidade final do veículo = " + velocity + "Km/h");
