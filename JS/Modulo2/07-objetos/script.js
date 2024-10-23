ba17be2957b5b3f746a9d03f7b8535b362d761c2
LauVii13 <viniciusglaureano@gmail.com>
2024-10-23 17:41:34 -0300

    Aula 7 finished

---
+++ JS/Modulo2/07-objetos/script.js
@@ -0,0 +1,23 @@
+let name = "prop";
+
+let objeto = {};
+
+objeto.prop = "Hello World!";
+
+console.log(objeto[nome]);
+
+let funcao = "log";
+console[funcao]("teste");
+
+const pessoa = {};
+pessoa.nome = "Vinícius";
+pessoa.idade = 19;
+
+pessoa.colegas = ["Pedro", "Enzo", "Danilo", "Gustavo"];
+pessoa.endereco = {
+  rua: "Rua dos bobos",
+  numero: 0,
+  bairro: "Engraçado",
+};
+
+console.log(pessoa);
