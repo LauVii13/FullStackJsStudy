4d284b286c1c37577378394afb5d60b4ae12e532
LauVii13 <viniciusglaureano@gmail.com>
2024-10-23 17:20:27 -0300

    matriz e console.table

--- JS/Modulo2/06-arrays-bidimensionais/script.js
+++ JS/Modulo2/06-arrays-bidimensionais/script.js
@@ -11,3 +11,11 @@ console.log(arr[0]);
 console.log(arr[1][2]);
 console.log(arr[4][0]);
 console.log(arr[4][1][1]);
+
+const matriz = [
+  ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+  ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+  ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+];
+
+console.table(matriz);
