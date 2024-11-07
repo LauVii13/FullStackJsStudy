28498f89b0d2c5ff2f6e08640125f7722c8518f3
LauVii13 <viniciusglaureano@gmail.com>
2024-11-07 19:15:55 -0300

    aula 08

---
+++ JS/Modulo6/08-assincronicidade/index.js
@@ -0,0 +1,14 @@
+function step02() {
+  console.log("Passo 02");
+}
+
+console.log("Passo 01");
+step02();
+console.log("Passo 03");
+console.log("Passo 04");
+
+setTimeout(() => {
+  console.log("Passo 05");
+}, 1000 * 2);
+
+console.log("Passo 06");
