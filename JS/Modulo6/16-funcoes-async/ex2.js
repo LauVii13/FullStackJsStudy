004699452efdde199825ccafb66a16764d38d37e
LauVii13 <viniciusglaureano@gmail.com>
2024-11-07 22:01:17 -0300

    aula 16

---
+++ JS/Modulo6/16-funcoes-async/ex2.js
@@ -0,0 +1,9 @@
+const numbers = [4, 9, 5, 13, 77];
+
+async function asyncSquare(x) {
+  return x * x;
+}
+
+Promise.all(numbers.map((number) => asyncSquare(number))).then((squares) => {
+  console.log(squares);
+});
---
+++ JS/Modulo6/16-funcoes-async/index.js
@@ -0,0 +1,18 @@
+async function asyncSum(a, b) {
+  return a + b;
+}
+
+async function asyncSubtract(a, b) {
+  return a - b;
+}
+
+const sumResult = asyncSum(50, 33);
+const subtractResult = asyncSubtract(50, 30);
+
+Promise.all([sumResult, subtractResult])
+  .then((results) => {
+    console.log(results);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
