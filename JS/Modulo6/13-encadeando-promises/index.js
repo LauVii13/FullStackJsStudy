df13a9d6147b82121b6d6dd85b09560f90bbb4e2
LauVii13 <viniciusglaureano@gmail.com>
2024-11-07 21:33:57 -0300

    aula 13

---
+++ JS/Modulo6/13-encadeando-promises/index.js
@@ -0,0 +1,40 @@
+function checkAge(age) {
+  return new Promise((resolve, reject) => {
+    if (age) resolve(age > 18);
+    else reject(new Error("age is required"));
+  });
+}
+
+function getAge(birthday) {
+  return new Promise((resolve, reject) => {
+    if (birthday) {
+      const birthYear = new Date(birthday).getFullYear();
+      const currentYear = new Date().getFullYear();
+      resolve(currentYear - birthYear);
+    } else reject(new Error("birthday is required"));
+  });
+}
+
+getAge("1999-02-02")
+  .then((age) => checkAge(age))
+  .then((isOver18) => {
+    if (isOver18) console.log("É maior de idade");
+    else console.log("É menor de idade");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+// .then((age) => {
+//   checkAge(age)
+//     .then((isOver18) => {
+//       if (isOver18) console.log("É maior de idade");
+//       else console.log("É menor de idade");
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// })
+// .catch((err) => {
+//   console.log(err.message);
+// });
