cac29a225d4955217ce3f041dec584d39294235f
LauVii <viniciusglaureano@gmail.com>
2024-11-21 17:24:29 -0300

    creating base structure

---
+++ NodeJS/Web/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/node_modules/15-exercicio-02/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("node:path");
+const router = require("./routes");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor Iniciado em http://localhost:" + PORT);
+});
