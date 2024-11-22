6ce5975be5d7048288c903d870bcb027454f2d4c
LauVii <viniciusglaureano@gmail.com>
2024-11-21 21:35:23 -0300

    aula 21 (POST)

--- NodeJS/Web/20-api-rest/src/controllers/games-controller.js
+++ NodeJS/Web/20-api-rest/src/controllers/games-controller.js
@@ -36,8 +36,40 @@ module.exports = {
   },
 
   // POST /games
+  save: (req, res) => {
+    const { name, genres, year } = req.body;
 
+    const newGame = {
+      id: Math.floor(Math.random() * 999999),
+      name,
+      genres,
+      year,
+    };
+
+    games.push(newGame);
+    res.status(201);
+    res.json(newGame);
+  },
   // PUT /games/:id
 
   // DELETE /games/:id
+
+  // POST /games/:id/genres
+  addGenre: (req, res) => {
+    const { id } = req.params;
+    const { genre } = req.body;
+
+    const gameIndex = games.findIndex((game) => game.id === +id);
+
+    if (gameIndex === -1) {
+      return res.status(404).json({ message: "Game not found!" });
+    }
+
+    if (typeof genre !== "string" || games[gameIndex].genres.includes(genre)) {
+      return res.status(400).json({ message: "Invalid genre!" });
+    }
+
+    games[gameIndex].genres.push(genre);
+    res.json(games[gameIndex]);
+  },
 };
--- NodeJS/Web/20-api-rest/src/server.js
+++ NodeJS/Web/20-api-rest/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const gamesController = require("./controllers/games-controller");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
@@ -9,6 +10,9 @@ app.get("/", (req, res) => {
 
 app.get("/games", gamesController.index);
 app.get("/games/:id", gamesController.show);
+
+app.post("/games", gamesController.save);
+app.post("/games/:id/genres", gamesController.addGenre);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
